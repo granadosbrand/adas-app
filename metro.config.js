3c8957045f9ad33a02d0563509759667de291ba3
Diego Granados <diego.granados@sonepar.co>
2025-09-16 22:43:47 -0500

    añadir configuracion para metros

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,11 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
+
 const config = getDefaultConfig(__dirname)
- 
+
+// --- burnt ---
+config.resolver.sourceExts.push("mjs");
+config.resolver.sourceExts.push("cjs");
+// --- end burnt ---
+
 module.exports = withNativeWind(config, { input: './app/global.css' })
